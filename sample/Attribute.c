3aaeb13f77f3737e05c6d025be7d225a85532342
RDA-DATA <rdadata@korea.kr>
2024-07-10 17:54:10 +0900

    2024.07.10 제공 파일 영문 설명 추가

--- sample/Attribute.c
+++ sample/Attribute.c
@@ -6,6 +6,17 @@
 #include "IotClient.h"
 #include "IotClient_ErrCode.h"
 
+/**
+ * A sample of connecting objects and transmitting data using the ITA method
+ *
+ *
+ * After accessing the PoC, search for the objects created in the Objects > Object Management menu and enter the information below.
+ * Create an authentication object using the entered information and receive the object through the IoTClient class provided by the SDK.
+ * Connect and activate the object to switch it to a state where it can receive data.
+ * To transmit the collected data, enter values that match the attribute name and attribute type of the object to create data in JSON format.
+ * Transmit the data through the object connected to the PoC.
+ */
+
 int main(int argc, char** argv)
 {
 
@@ -16,12 +27,14 @@ int main(int argc, char** argv)
 	char* siteId = "C000000037";
 	char* thingName = "999999.1324";
 
+    // Utilize the basic information of the object as parameters.
 	IotClient* client = createIotClient(authCode, userId, userPw, siteId, thingName);
 	if(client != NULL)
 	{
 		if( client->connect() == ERR_OK )
 		{ // connected
 
+            //Create data in JSON format and transmit it once every 10 seconds.
 			char* msgCode = NULL;
 			char* baseStr = "{\"sample1\": %d, \"sample2\": %d, \"sample3\": %d}";
 
--- sample/Connection.c
+++ sample/Connection.c
@@ -5,6 +5,15 @@
 #include "IotClient.h"
 #include "IotClient_ErrCode.h"
 
+/**
+ * A sample of connecting objects registered in the PoC using the ITA method
+ *
+ *
+ * After accessing the PoC, search for the objects created in the Objects > Object Management menu and enter the information below.
+ * Create an authentication object using the entered information and receive the object through the IoTClient class provided by the SDK.
+ * Connect and activate the device object.
+ */
+
 int main(int argc, char** argv)
 {
 	char* authCode =  "adf022d0824dc28c";
@@ -14,6 +23,7 @@ int main(int argc, char** argv)
 	char* siteId =  "C000000037";
 	char* thingName = "999999.1324";
 
+    // Utilize the basic information of the object as parameters.
 	IotClient* client = createIotClient(authCode, userId, userPw, siteId, thingName);
 	if(client != NULL)
 	{
