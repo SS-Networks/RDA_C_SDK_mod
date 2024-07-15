ce798631226e8f02f4de78599b35da5f40f69eef
RDA-DATA <rdadata@korea.kr>
2024-07-15 15:59:44 +0900

    2024.07.15 java SDK와 형식 동일하게 변경

--- sample/Attribute.c
+++ sample/Attribute.c
@@ -17,45 +17,36 @@
  * Transmit the data through the object connected to the PoC.
  */
 
-int main(int argc, char** argv)
-{
+int main(int argc, char **argv) {
 
-	char* authCode = "adf022d0824dc28c";
-	char* userId = "";
-    char* userPw = "";
-
-	char* siteId = "C000000037";
-	char* thingName = "999999.1324";
+    char *siteId = "C000000037";
+    char *thingName = "999999.1324";
+    char *authCode = "adf022d0824dc28c";
+    char *userId = "";
+    char *userPw = "";
 
     // Utilize the basic information of the object as parameters.
-	IotClient* client = createIotClient(authCode, userId, userPw, siteId, thingName);
-	if(client != NULL)
-	{
-		if( client->connect() == ERR_OK )
-		{ // connected
+    IotClient *client = createIotClient(authCode, userId, userPw, siteId, thingName);
+    if (client != NULL) {
+        if (client->connect() == ERR_OK) { // connected
 
             //Create data in JSON format and transmit it once every 10 seconds.
-			char* msgCode = NULL;
-			char* baseStr = "{\"sample1\": %d, \"sample2\": %d, \"sample3\": %d}";
+            char *msgCode = NULL;
+            char *baseStr = "{\"sample1\": %d, \"sample2\": %d, \"sample3\": %d}";
 
-			char dataStr[LONG_STR_LEN] = {0, };
+            char dataStr[LONG_STR_LEN] = {0,};
 
-			while(1) {
-				int value1 = rand() % 31 + 10;
-				int value2 = rand() % 31 + 10;
-				int value3 = rand() % 31 + 10;
-			
-				sprintf(dataStr, baseStr, value1, value2, value3);
-				client->sendAttributes(msgCode, dataStr);
-				
-				// 10 sec
-				sleep(10);
-			}
+            int value1 = 1.5;
+            int value2 = 2.2;
+            int value3 = 3.3;
 
-		}
+            sprintf(dataStr, baseStr, value1, value2, value3);
+            client->sendAttributes(msgCode, dataStr);
+        }
+        destroyIotClient(client);
+    }
 
-		destroyIotClient(client);
-	}
-
-	return 0;
+    return 0;
 }
+
+
